9075f306aa7a06a168d050adb662bb587dcde417
CharlieChass <arcochas@gmail.com>
2021-10-15 13:21:29 +0200

    Proyecto Creado

---
+++ app/src/main/java/com/murallaromana/dam/segundo/doritosprueba/SegundaActivity.kt
@@ -0,0 +1,11 @@
+package com.murallaromana.dam.segundo.doritosprueba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.segunda_activity)
+    }
+}
